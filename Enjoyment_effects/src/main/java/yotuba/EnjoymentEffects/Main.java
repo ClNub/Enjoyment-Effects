2028f54ffbd6065cb0b917ee2341ade4a61e9435
ClNub <Yotubachan@users.noreply.github.com>
2020-05-31 23:39:57 +0900

    ちょっとだけ書き直し

--- Enjoyment_effects/src/main/java/yotuba/EnjoymentEffects/Main.java
+++ Enjoyment_effects/src/main/java/yotuba/EnjoymentEffects/Main.java
@@ -1,21 +1,23 @@
 package yotuba.EnjoymentEffects;
 
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.event.EventHandler;
 import cn.nukkit.potion.InstantEffect;
 import cn.nukkit.event.potion.PotionApplyEvent;
 import cn.nukkit.potion.Effect;
+import cn.nukkit.entity.Entity;
 
 public class Main extends PluginBase {
     @Override
     public void onEnable(){
-        InstantEffect(24, "guilt", 44, 0, 28, true);
-        InstantEffect(25, "reflain", 210, 224, 255, true);
-        InstantEffect(26, "drain", 100, 0, 0);
+        new InstantEffect(24, "guilt", 44, 0, 28, true);
+        new InstantEffect(25, "reflain", 210, 224, 255, true);
+        new InstantEffect(26, "drain", 100, 0, 0);
     }
 
     @EventHandler
     public boolean onPotionApply(PotionApplyEvent event){
-        Effect effect = event.getAppleEffect();
+        Effect effect = event.getApplyEffect();
         Entity entity = event.getEntity();
         int duration =
 
@@ -30,7 +32,6 @@ public class Main extends PluginBase {
 
                 break;
         }
-
         return true;
     }
 }
